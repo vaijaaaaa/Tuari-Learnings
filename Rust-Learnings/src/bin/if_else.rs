4c974c45d30d66875c9ecafd5105c807d7a471c1
Vaijnath Patil <vaijuwalker111@gmail.com>
2026-04-23 10:28:27 +0530

    Add example of if-else statements in Rust

---
+++ Rust-Learnings/src/bin/if_else.rs
@@ -0,0 +1,30 @@
+fn main(){
+
+    if 7 > 5 {
+        println!("7 is greater than 5");
+    }
+
+
+
+    let age = 16;
+    if age >= 18 {
+        println!("You can vote");
+    }else{
+        println!("You are too young to vote.");
+    }
+
+
+
+    let score = 85;
+
+    if score >= 90 {
+        println!("Grade:A");
+    }else if score >= 80 {
+        println!("Grade : B");
+    }else if (score >= 70){
+        println!("Grade : C");
+    }else{
+        println!("Grade:F");
+    }
+
+} 
